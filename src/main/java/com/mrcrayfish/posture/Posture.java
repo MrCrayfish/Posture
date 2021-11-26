435e546f353a450d23061d0105cbe3b3b236a3ef
MrCrayfish <mrcrayfish@hotmail.com>
2021-11-27 03:27:40 +1030

    🐛 Fixed issue preventing clients joining servers that don't have the mod installed

--- src/main/java/com/mrcrayfish/posture/Posture.java
+++ src/main/java/com/mrcrayfish/posture/Posture.java
@@ -1,6 +1,9 @@
 package com.mrcrayfish.posture;
 
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,5 +15,9 @@ public class Posture
 {
     public static final Logger LOGGER = LogManager.getLogger("Posture");
 
-    public Posture() {}
+    public Posture()
+    {
+        // Prevents the client from being rejected when joining to a server
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
 }
